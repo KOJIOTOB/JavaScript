e7d09c49dfb5a15d72510d7df457e72e14bb8f15
KOJIOTOB <htc.kbc@gmail.com>
2022-02-01 00:05:55 +0200

    Initial commit

---
+++ Task 1/main.js
@@ -0,0 +1,55 @@
+/*
+let hello = `hello`;
+let owu = 'owu';
+let com = 'com';
+let ua = 'ua';
+let num1 = '1';
+let num10 = '10';
+let num999 = '-999';
+let num123 = '123';
+let num314 = '3.14';
+let num27 = '2.7';
+let num16 = '16';
+let tru = 'true';
+let fals = 'false';
+
+hello = 'world';
+owu = 'okten';
+com = 'www';
+ua = 'ukraine';
+num1 = 'one';
+num10 = 'ten';
+num999 = '999';
+num123 = '321';
+num27 = '7.2';
+num16 = '61';
+tru = 'this is true';
+fals = 'this is false';
+*/
+
+
+/*let firstName = 'Вiктор';
+let middleName = 'Сергійович';
+let lastName = 'Колотов';*/
+
+/*let firstName = 'Вiктор',
+    middleName = 'Сергійович',
+    lastName = 'Колотов';*/
+
+/*
+let person = {
+ firstName: "Вiктор",
+ middleName: "Сергійович",
+ lastName: "Колотов",
+ age: 30
+};
+*/
+
+
+//alert(`Вiтаю ${person["firstName"]} ${person["middleName"]} Тобi ${person["age"]} рокiв`);
+
+
+//console.log(lastName);
+//alert (person);
+
+//document.write("<h1>Hello World</h1>");
