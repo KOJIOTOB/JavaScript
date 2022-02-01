b9c2477dab4205a3db57c32b0324248e48d77716
KOJIOTOB <htc.kbc@gmail.com>
2022-02-01 23:20:20 +0200

    home_work_is_finished

---
+++ Home_Work/Task_1/main.js
@@ -0,0 +1,167 @@
+/*let hello = `hello`;
+let owu = 'owu';
+let com = 'com';
+let ua = 'ua';
+let num1 = '1';
+let num10 = '10';
+let num999 = '-999';
+let num123 = '123';
+let num314 = '3.14';
+let num27 = '2.7';
+let num16 = '16';
+let true = 'true';
+let false = 'false';*/
+
+/*console.log(hello);
+console.log(owu);
+console.log(com);
+console.log(num1);
+console.log(num10);
+console.log(num999);
+console.log(num123);
+console.log(num314);
+console.log(num27);
+console.log(num16);
+console.log(true);
+console.log(false);*/
+
+/*
+alert(hello);
+alert(owu);
+alert(com);
+alert(ua);
+alert(num1);
+alert(num10);
+alert(num999);
+alert(num123);
+alert(num314);
+alert(num27);
+alert(num16);
+alert(true);
+alert(false);
+*/
+
+/*
+document.write(hello);
+document.write(owu);
+document.write(com);
+document.write(ua);
+document.write(num1);
+document.write(num10);
+document.write(num999);
+document.write(num123);
+document.write(num314);
+document.write(num27);
+document.write(num16);
+document.write(true);
+document.write(false);
+*/
+
+
+
+/*hello = 'world';
+owu = 'okten';
+com = 'www';
+ua = 'ukraine';
+num1 = 'one';
+num10 = 'ten';
+num999 = '999';
+num123 = '321';
+num27 = '7.2';
+num16 = '61';
+true = 'this is true';
+false = 'this is false';*/
+
+/*console.log(hello);
+console.log(owu);
+console.log(com);
+console.log(ua);
+console.log(num1);
+console.log(num10);
+console.log(num999);
+console.log(num123);
+console.log(num27);
+console.log(num16);
+console.log(true);
+console.log(false);*/
+
+/*alert(hello);
+alert(owu);
+alert(com);
+alert(ua);
+alert(num1);
+alert(num10);
+alert(num999);
+alert(num123);
+alert(num27);
+alert(num16);
+alert(true);
+alert(false);*/
+
+/*
+document.write(hello);
+document.write(owu);
+document.write(com);
+document.write(ua);
+document.write(num1);
+document.write(num10);
+document.write(num999);
+document.write(num123);
+document.write(num27);
+document.write(num16);
+document.write(tru);
+document.write(fals);
+*/
+
+
+/*
+let firstName = 'Вiктор';
+let middleName = 'Сергійович';
+let lastName = 'Колотов';
+*/
+
+
+/*let person = {
+    firstName: "Вiктор",
+    middleName: "Сергійович",
+    lastName: "Колотов"
+};*/
+/*console.log(person)*/
+/*console.log(person.firstName);*/
+
+
+/*let person1 = {
+ firstName: "Вiктор",
+ middleName: "Сергійович",
+ lastName: "Колотов",
+ age: 30
+};
+
+alert(`Вiтаю ${person1["firstName"]} ${person1["middleName"]} Тобi ${person1["age"]} рокiв`);*/
+
+//document.write("<h1>Hello World</h1>");
+
+
+/*let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);*/
+
+/*
+console.log(5 < 6);
+console.log(5 > 6);
+console.log(5 >= 6);
+console.log(5 ==! 6)
+console.log(10 == 10);
+console.log(10 === 10);
+console.log(10 > 10);
+console.log(10 < 10);
+console.log(10 != 10);
+console.log(123 === '123')
+console.log(123 == '123')
+*/
+
+
