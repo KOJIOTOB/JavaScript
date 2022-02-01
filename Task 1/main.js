b7eb276dbef7e83c1f86508d3031533b22511dde
KOJIOTOB <htc.kbc@gmail.com>
2022-02-01 19:29:20 +0200

    class work

--- Task 1/main.js
+++ Task 1/main.js
@@ -36,6 +36,7 @@ let lastName = 'Колотов';*/
     middleName = 'Сергійович',
     lastName = 'Колотов';*/
 
+
 /*
 let person = {
  firstName: "Вiктор",
@@ -46,10 +47,83 @@ let person = {
 */
 
 
-//alert(`Вiтаю ${person["firstName"]} ${person["middleName"]} Тобi ${person["age"]} рокiв`);
+/*const array = [1,2,3,5,6,7,8,9,0,10];
+let result = array[0] + array[1] + array[2] + array[3] + array[4] + array[5]
+    + array[6] + array[7] + array[8] + array[9] + array[0] + array[10];
+console.log(result);*/
+
+//console.log(`Вiтаю ${person["firstName"]} ${person["middleName"]} Тобi ${person["age"]} рокiв`);
 
 
 //console.log(lastName);
 //alert (person);
 
 //document.write("<h1>Hello World</h1>");
+
+/*let bookJs1 = {
+    title: "JavaScript для детей",
+    pages: 288,
+    genre: "Cамоучитель по программированию"
+};
+console.log(bookJs1);
+*/
+
+
+/*
+let bookJs2 = {
+    title: "JavaScript для детей",
+    pages: 288,
+    genre: "Cамоучитель по программированию",
+    author: "Ник Морган"
+};
+console.log(bookJs2);
+*/
+
+/*
+const arr = [
+    {
+        title: "JavaScript для детей",
+        pages: 288,
+        genre: "Cамоучитель по программированию",
+        author: "Ник Морган",
+    },
+    {
+        title: "JavaScript",
+        pages: 388,
+        genre: "Cамоучитель"
+    },
+    {
+        title: "JavaScript Okten",
+        pages: 88,
+        genre: "Cамоучитель по программированию"
+    }
+];
+console.log(arr[0]);
+*/
+
+/*
+let height = 23;
+let width = 10;
+
+let s = height * width;
+console.log(s)
+*/
+
+/*
+let heightC = 10;
+let dC = 4;
+let s = Math.PI * (dC / 2) ** 2 * heightC;
+console.log(s)
+*/
+
+/*let n = 3
+let m = 4*/
+
+//let k = Math.sqrt((n**2 + m**2))
+
+/*let k = Math.sqrt((Math.pow(n, 2) + Math.pow(m, 2)))
+
+
+alert(k);*/
+
+
