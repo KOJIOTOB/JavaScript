67b2d07570d7659372f1b9e9bd36cbd1bd1d0dfd
KOJIOTOB <htc.kbc@gmail.com>
2022-02-06 10:25:17 +0200

    finished_task

--- Class_Work/Task_3/main.js
+++ Class_Work/Task_3/main.js
@@ -1,7 +1,7 @@
 
-let array = [2,17,13,6,22,31,45,66,100,-18];   // без изменений
+/*let array = [2,17,13,6,22,31,45,66,100,-18];   // без изменений*/
 
-let arrayBack = [2,17,13,6,22,31,45,66,100,-18];  // в обратном порядке
+/*let arrayBack = [2,17,13,6,22,31,45,66,100,-18];  // в обратном порядке*/
 
 /*1. перебрати його циклом while*/
 
@@ -14,6 +14,12 @@ while (i < array.length) {
 }
 */
 
+/*let i = arrayBack.length;
+
+while (i --) {
+    console.log(arrayBack[i]);
+}*/
+
 
 /*2. перебрати його циклом for*/
 
@@ -24,29 +30,51 @@ for (i = 0; i < array.length; i++) {
 }
 */
 
+/*for (let i = arrayBack.length-1; i >= 0; i--) {
+    console.log(arrayBack[i])
+}*/
+
 /*3. перебрати циклом while та вивести  числа тільки з непарним індексом*/
 
 /*
 let i = 0;
 
 while (i < array.length) {
-    if (i% 2 === 1) {
+    if (i % 2 === 1) {
         console.log(array[i])
     }
     i++
 }
 */
 
+/*let i = arrayBack.length;
+
+while (i--) {
+    if (i % 2 === 1) {
+        console.log(arrayBack[i])
+    }
+
+}*/
+
 /*
 4. перебрати циклом for та вивести  числа тільки з непарним індексом*/
 
 /*
 for (let i = 0; i < array.length; i++) {
-    if (i% 2 === 1) {
+    if (i % 2 === 1) {
         console.log(array[i])
     }
 }
 */
+
+/*
+for (let i = arrayBack.length-1; i >= 0; i--) {
+    if (i % 2 === 1) {
+        console.log(arrayBack[i])
+    }
+        }
+*/
+
 
 /*
 5. перебрати циклом while та вивести  числа тільки парні  значення*/
@@ -62,6 +90,14 @@ while (i < array.length) {
 }
 */
 
+/*let i = arrayBack.length;
+
+while (i--) {
+    if(i % 2 !== 1) {
+        console.log(arrayBack[i])
+    }
+}*/
+
 /*
 6. перебрати циклом for та вивести  числа тільки парні  значення*/
 
@@ -73,21 +109,36 @@ for ( let i = 0; i < array.length; i++) {
 }
 */
 
-
-/*
-7. замінити кожне число кратне 3 на слово "okten"*/
-
-/*
-for (let i = 0; i < array.length; i++) {
-    if ( array[i]%3 === 0) {
-        array[i] = "Okten";
-        console.log(array[i])
+/*for (let i = arrayBack.length-1; i >= 0; i--) {
+    if (i % 2 === 0) {
+        console.log(arrayBack[i])
     }
 }*/
 
 
 /*
+7. замінити кожне число кратне 3 на слово "okten"*/
+
+/*for (let i = 0; i < array.length; i++) {
+    if ( array[i]%3 === 0) {
+        array[i] = "okten";
+        console.log(array)
+    }
+}*/
+
+/*
+for (let i = arrayBack.length-1; i >= 0; i--) {
+    if (arrayBack[i]%3 === 0) {
+        arrayBack[i] = "okten";
+        console.log(arrayBack)
+    }
+}
+*/
+
+
+/*
 8. вивести масив в зворотньому порядку.*/
+
 
 /*
 let arrayBack = [2,17,13,6,22,31,45,66,100,-18];
