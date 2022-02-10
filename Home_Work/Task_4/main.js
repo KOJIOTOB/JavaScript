228ada6431449439709d9c534f4fbf3bb435edb5
KOJIOTOB <htc.kbc@gmail.com>
2022-02-10 09:42:36 +0200

    fix task

--- Home_Work/Task_4/main.js
+++ Home_Work/Task_4/main.js
@@ -1,17 +1,56 @@
 /*
-- створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б*/
+- створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
+*/
 
-/*
-function rectangle(a, b) {
+/*function areaRectangle(a, b) {
     let rect = a * b;
     return rect;
 }
 
-console.log(rectangle(50,50))
-*/
+console.log(areaRectangle(50,50))*/
 
 /*
 - створити функцію яка обчислює та повертає площу кола з радіусом r
 */
+
+/*function areaRad(r) {
+    let radius = r ** 2;
+    let xxx = 3.14 * radius;
+    return xxx;
+}
+
+console.log(areaRad(10))*/
+
+
+/*
+- створити функцію яка обчислює та повертає площу циліндру S= 2πRH
+*/
+
+/*
+function areaCylinder(r, h) {
+    let Cylinder = 2 * 3.14 * r * h;
+    return Cylinder;
+}
+
+console.log(areaCylinder(10,10))
+document.write('Площадь цылиндра ' + areaCylinder(10,10));*/
+
+/*
+- створити функцію яка приймає масив та виводить кожен його елемент
+*/
+
+/*let array = [12,32,65,878,234,12312,787,43535,54];
+function arrNum(array) {
+    for (let i = 0; i < array.length; i++) {
+        console.log(array[i])
+    }
+}
+arrNum(array)*/
+
+
+/*
+- створити функцію яка створює параграф з текстом. Текст задати через аргумент
+*/
+
 
 
